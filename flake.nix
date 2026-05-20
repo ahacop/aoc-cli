f615fd4ab51b8545de7809222734f785001572a6
Ara Hacopian <ara@hacopian.de>
2026-05-20 12:04:36 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "Advent of Code CLI";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system:
+        f (import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        }));
+    in
+    {
+      packages = forAllSystems (pkgs:
+        let
+          rust = pkgs.rust-bin.stable.latest.default;
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = rust;
+            rustc = rust;
+          };
+        in
+        {
+          default = rustPlatform.buildRustPackage {
+            pname = "aoc-cli";
+            version = "0.1.0";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+
+            meta = {
+              description = "Advent of Code CLI";
+              homepage = "https://github.com/ahacop/aoc-cli";
+              license = pkgs.lib.licenses.mit;
+              mainProgram = "aoc";
+            };
+          };
+        });
+
+      devShells = forAllSystems (pkgs:
+        let
+          rust = pkgs.rust-bin.stable.latest.default.override {
+            extensions = [ "rust-src" "rust-analyzer" "clippy" "rustfmt" ];
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              rust
+              pkgs.cargo-watch
+              pkgs.cargo-edit
+              pkgs.just
+            ];
+
+            RUST_BACKTRACE = 1;
+          };
+        });
+
+      formatter = forAllSystems (pkgs: pkgs.nixpkgs-fmt);
+    };
+}
